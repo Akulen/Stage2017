75c696368c47841a31178ada34c5d9f9fbed4707
Akulen <tomas@rigaux.com>
2017-06-23 15:02:13 +0200

    Clean utils

--- utils.py
+++ utils.py
@@ -1,49 +1,18 @@
+from math import sqrt
 import datetime
 import numpy as np
 import random
 import re
-import matplotlib.pyplot as plt
-from sklearn import metrics
-from math import sqrt
-
-def sq(x):
-    return x*x
-
-def square(x):
-    return x*x
 
 def custom_iso(clean=''):
     return re.sub('[\.].*', clean, datetime.datetime.now().isoformat())
 
-def selectBatch(data, batchSize, replace=True, unzip=True):
-    ret = []
-    for i in range(batchSize):
-        if replace:
-            j = random.randint(0, len(data)-1)
-        else:
-            j = random.randint(i, len(data)-1)
-        ret.append(data[j])
-        data[i], data[j] = data[j], data[i]
-    if unzip:
-        return map(list, zip(* data[:batchSize]))
-    return data[:batchSize]
-
 def evaluate(z, y):
-    z = np.array(z)
-    y = np.array(y)
+    z       = np.array(z)
+    y       = np.array(y)
     sqError = sq(z-y)
-    mse = np.mean(sqError)
-    X = [i for i in range(len(sqError))]
-    Y0 = [x for x in sqError]
-    Y1 = sqError
-    Y1.sort()
-    Y2 = [mse] * len(sqError)
-    #plt.plot(X, Y0)
-    #plt.plot(X, Y1)
-    #plt.plot(X, Y2)
-    #plt.show()
-    rmse = sqrt(mse)
-    devi = np.std(sqError)
+    rmse    = sqrt(np.mean(sqError))
+    devi    = np.std(sqError)
     return rmse, devi
 
 def getData(filename, nbX, nbY):
@@ -52,4 +21,16 @@ def getData(filename, nbX, nbY):
         raw = list(map(float, line.split()))
         data.append((raw[nbY:nbY+nbX], raw[0:nbY]))
     return data
+
+def selectBatch(data, batchSize, replace=True, unzip=True):
+    ret = []
+    for i in range(batchSize):
+        j = random.randint(0 if replace else i, len(data)-1)
+        ret.append(data[j])
+        if not replace:
+            data[i], data[j] = data[j], data[i]
+    return map(list, zip(* data[:batchSize])) if unzip else data[:batchSize]
+
+def sq(x):
+    return x*x
 
