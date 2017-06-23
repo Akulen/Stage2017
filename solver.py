3b65f20d39929a840f903d9832c916a2cc6c6bc3
Akulen <tomas@rigaux.com>
2017-06-23 14:39:08 +0200

    Clean solver

--- solver.py
+++ solver.py
@@ -1,16 +1,22 @@
+from abc import ABCMeta, abstractmethod
 import tensorflow as tf
 
 class Solver(object):
+    __metaclass__ = ABCMeta
+
     def __init__(self, id, run, nbInputs):
         self.id       = id
         self.run      = run
         self.nbInputs = nbInputs
 
+    @abstractmethod
     def train(self, data, validationData, nbEpochs=100, batchSize=32):
         pass
     
+    @abstractmethod
     def evaluate(self, data):
         pass
 
+    @abstractmethod
     def solve(self, x):
         pass
