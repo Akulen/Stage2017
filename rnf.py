ea569f05909228cf425ccd793d234c7655a88c9a
Akulen <tomas@rigaux.com>
2017-07-06 14:28:01 +0200

    moved rnf to new structure

--- rnf.py
+++ rnf.py
@@ -11,66 +11,84 @@ import tensorflow as tf
 import utils
 
 class RNF1(ParallelForest):
-    def __init__(self, nbInputs, maxProf, nbFeatures, nbIter=-1, sparse=True,
-            use_et=False, sess=None, pref=""):
-        self.nbInputs   = nbInputs
-        self.maxProf    = maxProf
-        self.nbFeatures = nbFeatures
-        self.sparse     = sparse
-        self.sess       = sess
-        self.use_et     = use_et
+    def __init__(self, nbInputs, maxProf, layerSize, useRelu=False,
+            fixMiddle=False, sess=None, debug=False, sparse=True, useEt=False,
+            nbJobs=8, nbIter=-1, pref=""):
+        super().__init__(nbIter=nbIter, nbJobs=nbJobs, pref=pref)
+        self.pref = ("sparse-" if sparse else "") + "random-neural-" + self.pref 
 
-        pp = "sparse-" if sparse else ""
-        super().__init__(nbIter, pref=pp + "random-neural-" + pref)
+        self.nbInputs  = nbInputs
+        self.maxProf   = maxProf
+        self.layerSize = layerSize
+        self.useRelu   = useRelu
+        self.fixMiddle = fixMiddle
+        self.sess      = sess
+        self.debug     = debug
+        self.sparse    = sparse
+        self.useEt     = useEt
+
+        self.initSolvers()
 
     def createSolver(self, id):
-        return RNF2(self.nbInputs, self.maxProf, self.nbFeatures, 1,
-                self.sparse, id, sess=self.sess)
+        return RNF2(self.nbInputs, self.maxProf, self.layerSize,
+                useRelu=self.useRelu, fixMiddle=self.fixMiddle, sess=self.sess,
+                debug=self.debug, sparse=self.sparse, useEt=self.useEt,
+                nbIter=1, pref=self.pref + "-" + str(id))
 
 
 
 class RNF2(Forest):
-    def __init__(self, nbInputs, maxProf, nbFeatures, nbIter=-1, sparse=True,
-            id=0, use_et=False, use_relu=False, sess=None, pref=""):
-        self.id         = id
-        self.nbInputs   = nbInputs
-        self.maxProf    = maxProf
-        self.nbFeatures = nbFeatures
-        self.sparse     = sparse
-        self.use_et     = use_et
-        self.use_relu   = use_relu
-        self.sess       = sess
-        self.layers     = [
-            (nbFeatures-1, 100),
-            (nbFeatures,     1)
+    def __init__(self, nbInputs, maxProf, layerSize, useRelu=False,
+            fixMiddle=False, sess=None, debug=False, sparse=True, useEt=False,
+            nbIter=-1, pref=""):
+        self.nbInputs    = nbInputs
+        self.maxProf     = maxProf
+        self.layers      = [
+            (layerSize-1, 100),
+            (layerSize,     1)
         ]
+        self.useRelu     = useRelu
+        self.fixMiddle   = fixMiddle
+        self.sess        = sess
+        self.debug       = debug
+        self.sparse      = sparse
+        self.useEt       = useEt
+        self.maxFeatures = (self.nbInputs+2) // 3
 
-        pp = "sparse-" if sparse else ""
-        super().__init__(nbIter, pref=pp + "random-neural-" + pref)
+        super().__init__(nbIter=nbIter, pref=pref)
+        self.pref = ("sparse-" if sparse else "") + "random-neural-" + self.pref 
 
-        self.dt = [DT(i, self.run, self.nbInputs, (self.nbInputs+2)//3, self.maxProf)
-                for i in range(self.nbIter)]
-        self.et = ExtraTreesRegressor(n_estimators=self.nbIter, max_depth=self.maxProf)
+        if useEt:
+            self.et = DT(self.sess, self.run, self.nbInputs, self.maxProf,
+                    self.maxFeatures, nbIter=self.nbIter,
+                    treeType=ExtraTreesRegressor)
+        else:
+            self.initSolvers()
+            self.rf = self.iters[:]
+
         self.iters = [None]
 
-    def train(self, data, validation, nbEpochs=100, logEpochs=False):
-        if not self.use_et:
+    def createSolver(self, id):
+        return DT(id, self.run, self.nbInputs, self.maxProf, self.maxFeatures)
+
+    def train(self, data, validation, nbEpochs=100, batchSize=32,
+            logEpochs=False):
+        if not self.useEt:
             for i in range(self.nbIter):
-                batch = utils.selectBatch(data, len(data)//3, replace=False, unzip=False)
-                self.dt[i].train(batch, validation, nbEpochs)
+                batch = utils.selectBatch(data, len(data)//3, replace=False,
+                        unzip=False)
+                self.rf[i].train(batch, validation, nbEpochs)
         else:
-            x, y = map(np.array, zip(* data))
-            y = [yy[0] for yy in y]
-            self.et.fit(x.reshape(-1, 1), y)
+            self.et.train(data, validation, nbEpochs=nbEpochs)
 
         connectivity = [[] for _ in range(2)]
         weight       = [[] for _ in range(3)]
         bias         = [[] for _ in range(3)]
 
         for i in range(self.nbIter):
-            if self.use_et:
-                self.dt[i].tree = self.et.estimators_[i]
-            c, w, b = utils.dt2nn(self.dt[i], self.nbInputs, self.layers[0][0],
+            if self.useEt:
+                self.rf[i].tree = self.et.tree.estimators_[i]
+            c, w, b = utils.dt2nn(self.rf[i], self.nbInputs, self.layers[0][0],
                     self.layers[1][0], self.nbIter)
 
             for j in range(3):
@@ -84,11 +102,13 @@ class RNF2(Forest):
                 for j in range(len(connectivity[i])):
                     connectivity[i][j] = np.ones(connectivity[i][j].shape)
 
-        self.iters[0] = NN(self.id, self.run, self.nbInputs, self.layers,
+        self.iters[0] = NN(self.pref, self.run, self.nbInputs, self.layers,
                 connectivity=connectivity, weight=weight, bias=bias,
-                sess=self.sess, pref=self.pref, use_relu=self.use_relu)
+                useRelu=self.useRelu, fixMiddle=self.fixMiddle, sess=self.sess,
+                debug=self.debug)
 
-        return self.iters[0].train(data, validation, nbEpochs, logEpochs=logEpochs)
+        return self.iters[0].train(data, validation, nbEpochs=nbEpochs,
+                batchSize=batchSize, logEpochs=logEpochs)
 
     def evaluate(self, data):
         return self.iters[0].evaluate(data)
