ebd9a9b5ba0c243986258201fadc8c66dca13f1f
Akulen <tomas@rigaux.com>
2017-07-06 10:44:13 +0200

    Moved up all logic, Parallel forest is currently useless

--- forest.py
+++ forest.py
@@ -10,15 +10,24 @@ class Forest(object):
     def __init__(self, nbIter=-1, pref=""):
         if nbIter == -1:
             nbIter = 30
+        if pref != "":
+            pref = "-" + pref
         self.nbIter = nbIter
-        self.pref   = pref + "forest"
+        self.pref   = "forest" + pref
         self.run    = utils.custom_iso()
-        self.iters  = [None for _ in range(self.nbIter)]
 
-    def train(self, data, validation, nbEpochs=100, batchSize=32,
-            logEpochs=False):
-        r = [it.train(data, validation, nbEpochs, logEpochs=logEpochs)
+    def initSolvers(self):
+        self.iters  = [self.createSolver(self.pref + str(i))
+                for i in range(self.nbIter)]
+
+    @abstractmethod
+    def createSolver(self, id):
+        pass
+
+    def train(self, data, validation, nbEpochs=100, logEpochs=False):
+        r = [it.train(data, validation, nbEpochs=nbEpochs, logEpochs=logEpochs)
                 for it in self.iters]
+
         if logEpochs:
             fns = [[0 for _ in range(len(r[0][0]))] for _ in range(len(r[0]))]
 
@@ -28,13 +37,19 @@ class Forest(object):
                         fns[t][x] += f[t][x] / len(r)
 
             return fns
+    
+    def solve(self, data):
+        z = []
+        for x in data:
+            res = [it.solve([x])[0][0] for it in self.iters]
+            z.append(sum(res) / self.nbIter)
+        return z
 
     def evaluate(self, data):
-        z = []
-        for x, y in data:
-            res = [it.solve([x])[0] for it in self.iters]
-            z.append(sum(res) / self.nbIter)
-        return utils.evaluate(z, [y[0] for _, y in data])
+        x = [_x for _x, _ in data]
+        y = [_y[0] for _, _y in data]
+        z = self.solve(x)
+        return utils.evaluate(z, y)
 
     def close(self):
         for it in self.iters:
@@ -44,39 +59,21 @@ class ParallelForest(Forest):
     __metaclass__ = ABCMeta
 
     def __init__(self, nbIter=-1, nbJobs=8, pref=""):
-        super().__init__(nbIter, "parallel-" + pref)
-        self.nbJobs     = nbJobs
+        super().__init__(nbIter, pref)
+        self.pref = "parallel-" + self.pref
 
-        for i in range(self.nbIter):
-            self.iters[i] = self.createSolver(self.pref + str(i))
+        self.nbJobs = nbJobs
 
-    def train(self, data, validation, nbEpochs=100, logEpochs=False):
-        #r = Parallel(n_jobs=self.nbJobs)(
-        #    delayed(self.iters[i].train)(data, validation, nbEpochs, logEpochs)
-        #        for i in range(self.nbIter)
-        #)
-        return super().train(data, validation, nbEpochs=nbEpochs,
-                logEpochs=logEpochs)
+    #def train(self, data, validation, nbEpochs=100, logEpochs=False):
+    #    r = Parallel(n_jobs=self.nbJobs)(
+    #        delayed(self.iters[i].train)(data, validation, nbEpochs, logEpochs)
+    #            for i in range(self.nbIter)
+    #    )
 
-    @abstractmethod
-    def createSolver(self, id):
-        pass
-
-    def evaluate(self, data):
-        x = [_x for _x, _ in data]
-        z = self.solve(x)
-        return utils.evaluate(z, [y[0] for _, y in data])
-
-    def solve(self, data):
-        z = [0] * len(data)
-        for j in range(len(data)):
-            x = data[j]
-            #r = Parallel(n_jobs=self.nbJobs)(
-            #    delayed(self.iters[i].solve)([x]) for i in range(self.nbIter)
-            #)
-            r = [self.iters[i].solve([x]) for i in range(self.nbIter)]
-            z[j] = sum([_r[0][0] for _r in r]) / self.nbIter
-        return z
+    #def solve(self, data):
+    #    r = Parallel(n_jobs=self.nbJobs)(
+    #        delayed(self.iters[i].solve)([x]) for i in range(self.nbIter)
+    #    )
 
 
 
