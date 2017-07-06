a50d5697d2df8a035fb556a48094ef91fa03506e
Akulen <tomas@rigaux.com>
2017-07-06 10:22:35 +0200

    Clean dt

--- dt.py
+++ dt.py
@@ -1,74 +1,87 @@
-from forest import Forest
-from math import sqrt
-from sklearn.tree import DecisionTreeRegressor
-from solver import Solver
+from forest           import Forest
+from math             import sqrt
+from sklearn.ensemble import RandomForestRegressor
+from solver           import Solver
 import numpy as np
 import random
 import utils
 
 class DT(Solver):
-    def __init__(self, id, run, nbInputs, maxFeatures, maxProf):
+    def __init__(self, id, run, nbInputs, maxProf, maxFeatures, nbIter=1,
+            treeType=RandomForestRegressor):
         super().__init__(id, run, nbInputs)
-        self.maxProf = maxProf
+        self.maxProf     = maxProf
         self.maxFeatures = maxFeatures
+        self.nbIter      = nbIter
 
-        self.tree = DecisionTreeRegressor(max_features=maxFeatures, max_depth=maxProf)
+        self.tree = treeType(n_estimators=nbIter, max_depth=maxProf,
+                max_features=maxFeatures)
 
-    def train(self, data, validation, nbEpochs=100, batchSize=-1):
+    def train(self, data, validation, nbEpochs=100):
         X, y = utils.zipData(data)
-        X = np.array(X)
         self.tree.fit(X, y)
 
     def evaluate(self, data):
         xs, ys = utils.zipData(data)
-        xs = np.array(xs)
         return utils.evaluate(self.tree.predict(xs), ys)
 
     def solve(self, x):
-        x = np.array(x)
         return self.tree.predict(x)
 
-    def makeTree(self):
-        father = [-1] * self.tree.tree_.node_count
-        side   = [0] * self.tree.tree_.node_count
-        for i in range(self.tree.tree_.node_count):
-            if self.tree.tree_.children_left[i] >= 0:
-                father[self.tree.tree_.children_left[i]]  = i
-                father[self.tree.tree_.children_right[i]] = i
-                side[self.tree.tree_.children_left[i]]    = -1.
-                side[self.tree.tree_.children_right[i]]   = 1.
+    def makeTree(self, id=0):
+        tree   = self.tree.estimators_[id].tree_
+        father = [-1] * tree.node_count
+        side   = [0] * tree.node_count
+        for i in range(tree.node_count):
+            if tree.children_left[i] >= 0:
+                father[tree.children_left[i]]  = i
+                father[tree.children_right[i]] = i
+                side[tree.children_left[i]]    = -1.
+                side[tree.children_right[i]]   = 1.
         return father, side
 
-    def indexNodes(self):
+    def indexNodes(self, id=0):
+        tree  = self.tree.estimators_[id].tree_
         nodes = []
-        for i in range(self.tree.tree_.node_count):
-            if self.tree.tree_.children_left[i] >= 0:
+        for i in range(tree.node_count):
+            if tree.children_left[i] >= 0:
                 nodes.append(i)
         return nodes
 
-    def indexLeafs(self):
+    def indexLeafs(self, id=0):
+        tree = self.tree.estimators_[id].tree_
         leafs = []
-        for i in range(self.tree.tree_.node_count):
-            if self.tree.tree_.children_left[i] < 0:
+        for i in range(tree.node_count):
+            if tree.children_left[i] < 0:
                 leafs.append(i)
         return leafs
 
 class RF(Forest):
     def __init__(self, nbInputs, maxProf, nbIter=-1, pref=""):
-        super().__init__(nbIter, "random-" + pref)
+        super().__init__(nbIter, pref)
+        self.pref     = "random-" + self.pref
         self.nbInputs = nbInputs
         self.maxProf  = maxProf
 
         maxFeatures   = (nbInputs + 2) // 3
         for i in range(self.nbIter):
-            self.iters[i] = DT(i, self.run, self.nbInputs, maxFeatures, self.maxProf)
+            self.iters[i] = DT(i, self.run, self.nbInputs, self.maxProf,
+                    maxFeatures)
 
-    def train(self, data, validation, nbEpochs=100, batchSize=-1):
+    def train(self, data, validation, nbEpochs=100, logEpochs=False):
         for it in self.iters:
-            batch = utils.selectBatch(data, len(data)//3, replace=False, unzip=False)
-            it.train(batch, validation, nbEpochs, batchSize)
+            batch = utils.selectBatch(data, len(data)//3, replace=False,
+                    unzip=False)
+            it.train(batch, validation, nbEpochs=nbEpochs)
+
+        if logEpochs:
+            fn = [[self.solve(x)
+                for x in np.linspace(0, 1, 10**3)]] * (nbEpochs+1)
+            return fn
 
     def solve(self, x):
+        if isinstance(x, list):
+            return [self.solve(_x) for _x in x]
         res = [it.solve(x)[0] for it in self.iters]
         z = sum(res) / self.nbIter
         return z
