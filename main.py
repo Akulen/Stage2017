7a00f9894674aab906bc5ef87b0266fd8fd5a533
Akulen <tomas@rigaux.com>
2017-07-18 11:21:56 +0200

    Add datasets and isolate computing to avoid memory leak

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from dn               import DN1, DN2
 from dt               import RF
 from joblib           import Parallel, delayed
 from math             import sqrt
@@ -9,6 +10,7 @@ from utils            import getData
 import numpy as np
 import random
 import sys
+import multiprocessing as mp
 
 def evaluateSolver(solver, data):
     random.shuffle(data)
@@ -19,7 +21,6 @@ def evaluateSolver(solver, data):
 
     solver.train(trainData, validationData)
     eval = solver.evaluate(testData)
-    solver.close()
     return eval
 
 datafiles = [("autoMPG",     7  ),
@@ -27,46 +28,58 @@ datafiles = [("autoMPG",     7  ),
              ("communities", 101),
              ("forest",      10 ),
              ("wisconsin",   32 ),
-             ("concrete",    8  )]
+             ("concrete",    8  ),
+             ("friedman1",   10 ),
+             ("hwang5n",     2  )]
 
-solvers = [lambda id, n, sess : NNF2(n, nbNeurones, sess=sess, nbIter=nbIter,
+solvers = [lambda id, n, sess : NNF2(n, nbNeurones, sess=sess, debug=debug,
+                nbIter=nbIter, pref=str(id)),
+           lambda id, n, sess : RF(n, nbNeurones, nbIter=nbIter,
                 pref=str(id)),
-           lambda id, n, sess : RF(n, nbNeurones, nbIter=nbIter, pref=str(id)),
            lambda id, n, sess : RNF1(n, maxProf, nbNeurones, sess=sess,
-                nbIter=nbIter, pref=str(id)),
+                debug=debug, nbIter=nbIter, pref=str(id)),
            lambda id, n, sess : RNF2(n, maxProf, nbNeurones, sess=sess,
-                nbIter=nbIter, pref=str(id)),
+                debug=debug, nbIter=nbIter, pref=str(id)),
            lambda id, n, sess : RNF1(n, maxProf, nbNeurones, sess=sess,
-                sparse=False, nbIter=nbIter, pref=str(id)),
+                sparse=False, debug=debug, nbIter=nbIter, pref=str(id)),
            lambda id, n, sess : RNF2(n, maxProf, nbNeurones, sess=sess,
-                sparse=False, nbIter=nbIter, pref=str(id))]
+                sparse=False, debug=debug, nbIter=nbIter, pref=str(id)),
+           lambda id, n, sess : DN1(n, maxProf, nbNeurones, sess=sess,
+                debug=debug, nbIter=nbIter, pref=str(id)),
+           lambda id, n, sess : DN2(n, maxProf, nbNeurones, sess=sess,
+                debug=debug, nbIter=nbIter, pref=str(id))]
 assert len(sys.argv) > 1
 iSolver = int(sys.argv[1])
 assert iSolver < len(solvers)
 
 maxProf    = 6
 nbNeurones = 2**maxProf
-nbIter     = 3
+nbIter     = 5
+debug      = False
 
 def createSolver(id, nbInputs, sess):
     solver = solvers[iSolver](id, nbInputs, sess)
     return solver
 
 def thread(id, filename, nbInputs):
-    sess   = Session()
-    solver = createSolver(id, nbInputs, sess)
-    res    = evaluateSolver(solver, getData(filename, nbInputs, 1))
-    solver.close()
+    with Session() as sess:
+        with createSolver(id, nbInputs, sess) as solver:
+            res = evaluateSolver(solver, getData(filename, nbInputs, 1))
     return res
 
 if __name__ == '__main__':
-    for i, (filename, nbInputs) in enumerate(datafiles):
-        print("## " + filename)
+    for i, (filename, nbInputs) in enumerate(datafiles[:1]):
+        print("## %-15s" % filename, end='')
 
-        rmse = Parallel(n_jobs=1)(
-            delayed(thread)(j, filename, nbInputs) for j in range(3)
-        )
+        def foo():
+            rmse = Parallel(n_jobs=4)(
+                delayed(thread)(filename + "-" + str(j), filename, nbInputs) for j in range(3)
+            )
 
-        print("%5.2f (" % (sum(rmse) / len(rmse)), end='')
-        print("%5.2f)" % np.std(rmse))
+            print("%5.2f (" % (sum(rmse) / len(rmse)), end='')
+            print("%5.2f)" % np.std(rmse))
+
+        proc = mp.Process(target=foo)
+        proc.start()
+        proc.join()
 
